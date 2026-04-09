dd9da9aad31bfef4fe28f6a3a3f3dc618f2ce077
George Nunes <georgebnunes@gmail.com>
2026-04-09 13:53:46 -0300

    feat: service full implementation

--- internal/service/todo_service.go
+++ internal/service/todo_service.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/georgebnunes/todolist-with-go-and-mongodb/internal/domain"
 	"github.com/georgebnunes/todolist-with-go-and-mongodb/internal/repository"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
@@ -22,31 +24,80 @@ type TodoService interface {
 }
 
 type todoService struct {
-	repo *repository.TodoRepository
+	repo repository.TodoRepository
 }
 
-func NewTodoService(repo *repository.TodoRepository) TodoService {
+func NewTodoService(repo repository.TodoRepository) TodoService {
 	return &todoService{
 		repo: repo,
 	}
 }
 
 func (s *todoService) Create(ctx context.Context, todo domain.Todo) (domain.Todo, error) {
-	return domain.Todo{}, nil
+	if todo.Title == "" {
+		return domain.Todo{}, nil
+	}
+	return s.repo.Create(ctx, todo)
+
 }
 
 func (s *todoService) FindAll(ctx context.Context) ([]domain.Todo, error) {
-	return []domain.Todo{}, nil
+	return s.repo.FindAll(ctx)
 }
 
 func (s *todoService) FindByID(ctx context.Context, id string) (domain.Todo, error) {
-	return domain.Todo{}, nil
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return domain.Todo{}, ErrTodoNotFound
+	}
+
+	todo, err := s.repo.FindByID(ctx, objID)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return domain.Todo{}, ErrTodoNotFound
+		}
+		return domain.Todo{}, err
+	}
+
+	return todo, nil
+
 }
 
 func (s *todoService) Update(ctx context.Context, id string, todo domain.Todo) (domain.Todo, error) {
-	return domain.Todo{}, nil
+	if todo.Title == "" {
+		return domain.Todo{}, ErrTitleRequired
+	}
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return domain.Todo{}, ErrTodoNotFound
+	}
+
+	_, err = s.repo.FindByID(ctx, objID)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return domain.Todo{}, ErrTodoNotFound
+		}
+		return domain.Todo{}, err
+	}
+
+	return s.repo.Update(ctx, objID, todo)
 }
 
 func (s *todoService) Delete(ctx context.Context, id string) error {
-	return nil
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return ErrTodoNotFound
+	}
+
+	_, err = s.repo.FindByID(ctx, objID)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return ErrTodoNotFound
+		}
+		return err
+	}
+
+	return s.repo.Delete(ctx, objID)
 }
